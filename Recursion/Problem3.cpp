8b56c62409aaccf846f89d7f07e98936d8581c64
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2022-01-31 02:41:25 +0600

    All possible subsets

---
+++ Recursion/Problem3.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double eps = 1e-9;
+double pi = acos(-1);
+
+int fx[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+int fy[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int pop_cnt(int n) {return __builtin_popcount(n);}
+int clz(int n) {return __builtin_clz(n);}
+int ctz(int n) {return __builtin_ctz(n);}
+bool parity(int n) {return __builtin_parity(n);}
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define en "\n"
+#define ff first
+#define ss second
+#define sp(x) fixed << setprecision(x)
+#define VECT(v)  vector<int>v
+#define SCAN(v)  int temp; for(int i=0; i<n; i++) {cin>>temp; v.push_back(temp);}
+#define PRINT(v) for(int i = 0; i < v.size(); i++) cout << v[i] << " "; cout << en;
+#define SORT(v)  sort(v.begin(), v.end());
+#define RSORT(v) sort(v.begin(), v.end(), greater<int>())
+#define CASEP(v) cout<<"Case "<<tc<<": "<<v<<"\n"
+#define DEBUG(v) cout << v << " "; cout << en;
+#define MIN(a, b) a < b ? a : b
+#define MAX(a, b) a > b ? a : b
+#define all(v) v.begin(), v.end()
+#define mem(a, b) memset(a, b, sizeof(a))
+#define get_bit(num, pos) {num & (1 << pos)}
+#define set_bit(num, pos) (num |= (1 << pos))
+#define reset_bit(num, pos) {num &= ~(1 << pos)}
+#define toggle_bit(num, pos) {num ^= (1 << pos)}
+#define valid(nx, ny, row, col) nx >= 0 && nx < row && ny >= 0 && ny < col
+#define pii pair <int, int>
+#define inf int(2e9)
+#define mod int(1e9 + 7)
+
+int n;
+
+void work(vector <int> v, int i)
+{   
+    for(int j = i; j < n; j++)
+    {
+        v.push_back(j);
+        work(v, j + 1);
+        v.pop_back();
+    }
+
+    for(auto i : v)
+        cout << i << " ";
+    cout << en;
+}
+
+int main(void)
+{
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt", "r", stdin);
+        freopen("out.txt", "w", stdout);
+    #endif
+
+    fast_io
+
+    cin >> n;
+    vector <int> v;
+    work(v, 0);
+
+    return 0;
+}
